894acaaa3555b79fb879aff7ebd2eed9c32ddc4d
Nisha R S <nisha021rs@gmail.com>
2026-04-18 08:56:51 +0530

    Starting with basics

---
+++ try1.py
@@ -0,0 +1,5 @@
+import numpy as np 
+vector_row = np.array([1,2,3]) #create a vector as row
+vector_column = np.array([[1],[2],[3]])  #create a vector as column
+print(vector_row)
+print(vector_column)

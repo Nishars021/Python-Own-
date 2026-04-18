a2cf43b062ac97cf1c774afdda9f7b6744a1886f
Nisha R S <nisha021rs@gmail.com>
2026-04-19 00:15:59 +0530

    Getting ahead

--- try1.py
+++ try1.py
@@ -1,6 +1,13 @@
 import numpy as np 
-vector_row = np.array([1,2,3]) #create a vector as row
-vector_column = np.array([[1],[2],[3]])  #create a vector as column
-print(vector_row)
-print(vector_column)
+from scipy import sparse
+# vector_row = np.array([1,2,3]) #create a vector as row
+# vector_column = np.array([[1],[2],[3]])  #create a vector as column
+# print(vector_row)
+# print(vector_column)
 
+#create a matrix
+matrix = np.array([[0,0],[0,1],[3,0]])
+
+#Create a compressed sparsh row matrix
+matrix_sparse = sparse.csr_matrix(matrix)
+print(matrix_sparse)

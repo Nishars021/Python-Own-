a33afc28c4ea14c421b1c6b7a8971a636b972cb8
Nisha R S <nisha021rs@gmail.com>
2026-04-23 23:10:22 +0530

    Trying some basics

--- try1.py
+++ try1.py
@@ -1,13 +1,21 @@
 import numpy as np 
-from scipy import sparse
-# vector_row = np.array([1,2,3]) #create a vector as row
+# from scipy import sparse
+vector = np.array([1,2,3]) #create a vector as row
+print(vector[::-1]) #Reverse the row
+
 # vector_column = np.array([[1],[2],[3]])  #create a vector as column
 # print(vector_row)
 # print(vector_column)
 
 #create a matrix
-matrix = np.array([[0,0],[0,1],[3,0]])
+# matrix = np.array([[0,0],[0,1],[3,0]])
 
-#Create a compressed sparsh row matrix
-matrix_sparse = sparse.csr_matrix(matrix)
-print(matrix_sparse)
+# #Create a compressed sparsh row matrix
+# matrix_sparse = sparse.csr_matrix(matrix)
+# print(matrix_sparse)
+
+# vector = np.zeros(shape=5)
+# print(vector)
+
+# matrix=np.full(shape=(2,5), fill_value=1)
+# print(matrix)

5cd47a262645152ec037d45632e7c2f7b6b80aa4
Nisha R S <nisha021rs@gmail.com>
2026-05-27 23:52:43 +0530

    Performing basics with using w3school as reference to study

--- basics.py
+++ basics.py
@@ -25,7 +25,18 @@
 # print(A)
 # print(a)
 
-x, y, z = "Orange", "Banana", "Cherry"
-print(x)
-print(y)
-print(z)
+# fruits = ["Orange", "Banana", "Cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+
+x = 1
+y = 2
+z = 5
+print(x, y, z)

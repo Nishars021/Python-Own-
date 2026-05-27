f6308fe0e6dc6e84415bfbdf702a7ddd3cde1dfe
Nisha R S <nisha021rs@gmail.com>
2026-05-27 22:21:26 +0530

    Performing basics

--- basics.py
+++ basics.py
@@ -2,11 +2,15 @@
 #  print("5 is greater than 2")
 #  print("a is b")
 
-a=2
-b="Nisha"
-c='a'
-d=0.244
-print(a)
-print(b)
-print(c)
-print(d)
+# a=2
+# b="Nisha"
+# c='a'
+# d=0.244
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")

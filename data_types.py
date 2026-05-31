e0b581e3766e82339a607863a480249f3ee4cfd0
Nisha R S <nisha021rs@gmail.com>
2026-05-31 21:55:09 +0530

    Trying different data types.

--- data_types.py
+++ data_types.py
@@ -3,9 +3,13 @@ b=2.56
 c='nisha'
 d=True
 e=263j
+f=["Apple","Banana"]
+g=("Apple","Banana")
 
 print(a," is a ",type(a)," data type")
 print(b," is a ",type(b)," data type")
 print(c," is a ",type(c)," data type")
 print(d," is a ",type(d)," data type")
 print(e," is a ",type(e)," data type")
+print(f," is a ",type(f)," data type")
+print(g," is a ",type(g)," data type")

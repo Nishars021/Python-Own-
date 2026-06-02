9b40ce058c020b3f9d78e20c78d7e65bcc7f504a
Nisha R S <nisha021rs@gmail.com>
2026-06-02 20:31:44 +0530

    Tried all data types

--- data_types.py
+++ data_types.py
@@ -1,10 +1,18 @@
-a=23
-b=2.56
-c='nisha'
-d=True
-e=263j
-f=["Apple","Banana"]
-g=("Apple","Banana")
+a=23 #Integer
+b=2.56 #Float
+c='nisha' #String
+d=range(6) #Range
+e=263j #Complex
+f=["Apple","Banana"] #List
+g=("Apple","Banana") #Tuple
+h={"name" : "Nisha", "age" : 20, "Gender" : "Female"} #Dict
+i={"Apple","Banana"} #Set
+j=frozenset({"Apple","Banana"}) #Frozenset
+k=b"Hello" #Byte
+l=memoryview(bytes(6)) #Memoryview
+m=bytearray(3) #Bytearray
+n=True #Boolean
+o=None #None Type
 
 print(a," is a ",type(a)," data type")
 print(b," is a ",type(b)," data type")
@@ -13,3 +21,12 @@ print(d," is a ",type(d)," data type")
 print(e," is a ",type(e)," data type")
 print(f," is a ",type(f)," data type")
 print(g," is a ",type(g)," data type")
+print(h," is a ",type(h)," data type")
+print(i," is a ",type(i)," data type")
+print(j," is a ",type(j)," data type")
+print(k," is a ",type(k)," data type")
+print(l," is a ",type(l)," data type")
+print(m," is a ",type(m)," data type")
+print(n," is a ",type(n)," data type")
+print(o," is a ",type(o)," data type")
+

304b7e459625133505d8ea8f9e6083c371248157
Nisha R S <nisha021rs@gmail.com>
2026-06-02 23:23:12 +0530

    Tried & Learnt about diff data types.

--- data_types.py
+++ data_types.py
@@ -1,32 +1,49 @@
-a=23 #Integer
-b=2.56 #Float
-c='nisha' #String
-d=range(6) #Range
-e=263j #Complex
-f=["Apple","Banana"] #List
-g=("Apple","Banana") #Tuple
-h={"name" : "Nisha", "age" : 20, "Gender" : "Female"} #Dict
-i={"Apple","Banana"} #Set
-j=frozenset({"Apple","Banana"}) #Frozenset
-k=b"Hello" #Byte
-l=memoryview(bytes(6)) #Memoryview
-m=bytearray(3) #Bytearray
-n=True #Boolean
-o=None #None Type
+# a=23 #Integer
+# b=2.56 #Float
+# c='nisha' #String
+# d=range(6) #Range
+# print(list(d))
+# e=263j #Complex
+# f=["Apple","Banana"] #List
+# f.append("Apple")
+# print(f)
+# g=("Apple","Banana") #Tuple
+# # g[0]="Orange"
+# print(g)
+# h={"name" : "Nisha", "age" : 20, "Gender" : "Female"} #Dict
+# i={"Apple","Banana"} #Set
+# i.add("Banana")
+# print(i)
+# j=frozenset({"Apple","Banana"}) #Frozenset
+# j.append("Orange")
+# print(j)
+# k=bytes([10,2,30,40]) #Byte
+# print(k)
+# l=memoryview(bytes(5)) #Memoryview
+# print(list(l))
+# m=bytearray(5) #Bytearray
+# m[0] = 5
+# # m[1] = 66
+# # m[2] = 67
+# # m[3] = 68
+# # m[4] = 69
+# print(m)
+# n=True #Boolean
+# o=None #None Type
 
-print(a," is a ",type(a)," data type")
-print(b," is a ",type(b)," data type")
-print(c," is a ",type(c)," data type")
-print(d," is a ",type(d)," data type")
-print(e," is a ",type(e)," data type")
-print(f," is a ",type(f)," data type")
-print(g," is a ",type(g)," data type")
-print(h," is a ",type(h)," data type")
-print(i," is a ",type(i)," data type")
-print(j," is a ",type(j)," data type")
-print(k," is a ",type(k)," data type")
-print(l," is a ",type(l)," data type")
-print(m," is a ",type(m)," data type")
-print(n," is a ",type(n)," data type")
-print(o," is a ",type(o)," data type")
+# print(a," is a ",type(a)," data type")
+# print(b," is a ",type(b)," data type")
+# print(c," is a ",type(c)," data type")
+# print(d," is a ",type(d)," data type")
+# print(e," is a ",type(e)," data type")
+# print(f," is a ",type(f)," data type")
+# print(g," is a ",type(g)," data type")
+# print(h," is a ",type(h)," data type")
+# print(i," is a ",type(i)," data type")
+# print(j," is a ",type(j)," data type")
+# print(k," is a ",type(k)," data type")
+# print(l," is a ",type(l)," data type")
+# print(m," is a ",type(m)," data type")
+# print(n," is a ",type(n)," data type")
+# print(o," is a ",type(o)," data type")
 

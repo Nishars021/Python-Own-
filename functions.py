11fc58b05eb6d27d780d6a8757008778c58415c5
Nisha R S <nisha021rs@gmail.com>
2026-06-09 21:01:31 +0530

    Performing functions

--- functions.py
+++ functions.py
@@ -3,3 +3,13 @@
 # sum=a(10,20)
 # print(sum)
 
+def name(name,uni_name,cur_year,grad_year):
+    print(f"My name is {name}")
+    print(f"Current studying in {uni_name}")
+    print(f"I am in my {cur_year}nd year")
+    print(f"Graduating by {grad_year}")
+
+name("Nisha","Amity University",2,2028)
+name("Harsha","Amity University",0,2030)
+name("Harshita","Amity University",0,2033)
+name("Parnika","---",0,0)

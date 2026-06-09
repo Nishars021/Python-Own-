414c217d5535148c4d840dd03019fb9490d0190a
Nisha R S <nisha021rs@gmail.com>
2026-06-09 22:04:46 +0530

    Trying functions

--- functions.py
+++ functions.py
@@ -1,15 +1,14 @@
-# def a(a,b):
-#     print(a+b)
-# sum=a(10,20)
-# print(sum)
+def sum(a,b):
+    print(a+b)
+print(sum(10,20))
 
-def name(name,uni_name,cur_year,grad_year):
-    print(f"My name is {name}")
-    print(f"Current studying in {uni_name}")
-    print(f"I am in my {cur_year}nd year")
-    print(f"Graduating by {grad_year}")
+# def name(name,uni_name,cur_year,grad_year):
+#     print(f"My name is {name}")
+#     print(f"Current studying in {uni_name}")
+#     print(f"I am in my {cur_year}nd year")
+#     print(f"Graduating by {grad_year}")
 
-name("Nisha","Amity University",2,2028)
-name("Harsha","Amity University",0,2030)
-name("Harshita","Amity University",0,2033)
-name("Parnika","---",0,0)
+# name("Nisha","Amity University",2,2028)
+# name("Harsha","Amity University",0,2030)
+# name("Harshita","Amity University",0,2033)
+# name("Parnika","---",0,0)
